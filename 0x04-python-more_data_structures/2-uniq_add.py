a4bc2e555f79e776c95bd71cd8b29acfcacf82dd
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-09 03:39:05 +0200

    uploading full task

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    list = my_list.copy()
+    sum = 0
+    for i in list:
+
+
+    return sum
+
+
+
+
+
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print(result)
