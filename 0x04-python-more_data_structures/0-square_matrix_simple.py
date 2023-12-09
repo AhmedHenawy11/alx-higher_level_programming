657ed7985032689281401945613f48ad0c0ae341
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-09 03:00:27 +0200

    nostalgically grabber

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    l = 0
+    r = 0
+    for list in matrix:
+        new_matrix.append(list)
+    print(new_matrix[l][r])
+    for ls in matrix:
+        r = 0
+        for i in ls:
+            new_matrix[l][r] = pow(i, 2)
+            r += 1
+        l += 1
+    return new_matrix
