12ae604bcf99cbc73bc48dd84daabfe257d3d6ce
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-19 04:53:39 +0200

    confronts voicemail

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,23 +6,3 @@ def safe_print_integer_err(value):
     except (TypeError, ValueError):
         print("Exception: Unknown format code 'd' for object of type 'str'")
         return (False)
-
-
-
-
-
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
