36f7169652610658e45fa27f2e555c381fa70501
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-19 04:43:13 +0200

    newt Hayden

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,25 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         div = None
     finally:
-        print(f"Inside result: {div}")
+        print("Inside result: {}".format(div))
         return (div)
-
-
-
-
-
-
-
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-
