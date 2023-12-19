52fcafa998b834a3386dae5d9d4762d6ae3a88df
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-19 04:00:47 +0200

    martens overages

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    length = 0
-    for n in range(0, x):
+    val = 0
+    for a in range(0, x):
         try:
-            print(f"{my_list[n]}", end="")
-            length += 1
+            print("{:d}".format(my_list[a]), end="")
+            val += 1
         except (ValueError, TypeError):
             continue
     print("")
-    return (length)
+    return (val)
