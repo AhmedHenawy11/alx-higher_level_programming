2386fb44484626ff3324a6bb15480541a6ba260e
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-19 03:41:30 +0200

    carfare isometric

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    length = 0
+    for n in range(0,x):
+        try:
+            print(f"{my_list[n]}", end="")
+            length += 1
+        except IndexError:
+            break
+    print("")
+    return (length)
