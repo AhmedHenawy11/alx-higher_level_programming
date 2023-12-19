967e18cc90fa6c96501fa5d700bb16f7856bbe28
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-19 04:00:06 +0200

    impetuses Westphalia's

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    length = 0
+    for n in range(0, x):
+        try:
+            print(f"{my_list[n]}", end="")
+            length += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (length)
