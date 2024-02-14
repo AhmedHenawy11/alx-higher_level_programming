d3a5035b13c9cddcd7fa4d875eed6fb659aee1ea
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-02-14 23:07:31 +0200

    visual Romanies

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -6,10 +6,13 @@ module contains one class
 
 class Rectangle:
     """ class Rectangle """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ instantiation """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
