a59449d182d2b47ee152bb062efaf7e1f1622975
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-02-15 03:40:57 +0200

    cowhides surprisingly

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
     number_of_instances = 0
 
     print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """ instantiation """
         self.width = width
@@ -44,29 +45,29 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """ returns the area of the rectangle """
+        """ returns area of rectangle """
         return self.__height * self.__width
 
     def perimeter(self):
-        """ returns the perimeter of the rectangle """
+        """ returns perimeter of the rectangle """
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__height + self.__width) * 2
 
     def __str__(self):
-        """ returns a printable string of the class """
+        """ returns a printable string when print is called """
         ch = ""
         if self.__width == 0 or self.__height == 0:
             return ch
         for i in range(self.__height):
             for j in range(self.__width):
-                ch += "#"
+                ch += str(self.print_symbol)
             ch += '\n'
         return ch[:-1]
 
     def __repr__(self):
-        """ returns a string that creates a new object after evaluation """
-        return f"Rectangle({self.width}, {self.height})"
+        """ returns a string that creates a new object when evaluated """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         """ called when an object is deleted with del """
