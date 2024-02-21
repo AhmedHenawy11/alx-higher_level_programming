1c6c69aa0fcb8bd108d10a604991518cbcf4503c
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-02-21 04:49:13 +0200

    spying curry

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,11 @@ This module contains one function
 """
 
 
-def write_file(filename="", text=""):
-    """ appends  a text into file and return ist length , and creates the file if it doesn't exist"""
-    with open(filename, 'a') as file:
-        return file.write(text)
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open(filename, 'a') as f:
+        ap = f.write(text)
+    return ap
