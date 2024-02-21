6f00bf79b8d2395db186246e05a60dbf8ea57101
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-02-21 04:36:56 +0200

    null DMZ

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-This module contains one function
-"""
-
 
 def read_file(filename=""):
     """
     function that print the content of file
     :param filename: a file to be passed and print its outpu
     """
-    with open(filename) as file:
-        for line in file:
-            print(line, end="")
+    with open(filename, 'r') as file:
+        print(file.read())
