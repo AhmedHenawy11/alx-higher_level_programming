55dbd18877e1214c3ddb4f587ebdf19229653f2c
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-02-21 04:48:10 +0200

    arpeggios euthanasia

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,6 @@ This module contains one function
 
 
 def write_file(filename="", text=""):
-    """ appends  a text into file and return ist length """
+    """ writes  a new text into file and return ist length """
     with open(filename, 'w') as file:
         return file.write(text)
