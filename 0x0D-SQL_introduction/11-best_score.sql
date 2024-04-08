7401569c1a86c9359631e37d5d99c243da1ccfe4
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-04-08 05:52:22 +0200

    civets holding

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,3 @@
 -- lists all records with a score >= 10 in the table 
 
-SELECT score FROM second_table WHERE score >= 10
-ORDER BY score DESC;
+SELECT score FROM second_table WHERE score >= 10 ORDER BY score DESC;
