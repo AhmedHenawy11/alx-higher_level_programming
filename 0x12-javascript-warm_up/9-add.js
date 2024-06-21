45e2143f6d9d84d3f20aa86efa109978c2aa0680
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-06-21 05:40:04 +0300

    Orwell cowlick's

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('process');
+const argc = argv.length;
+
+function add (a, b) {
+    return parseInt(a) + parseInt(b); 
+}
+
+
+console.log(argc === 3 ? add(argv[2], argv[3]) : "NaN" );
