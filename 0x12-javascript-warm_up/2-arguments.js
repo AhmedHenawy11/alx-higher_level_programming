94d98efe2e87c23ae227c16dc7847e326204d72e
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-06-21 04:52:17 +0300

    grandmother Jove

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,10 @@
 const { argv } = require('process');
 const argc = argv.length;
 
-if (argc > 2) {
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
