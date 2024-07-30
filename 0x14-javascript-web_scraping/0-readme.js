2680e2ef445f9c1c5ce5bc77f06615059d94fcf0
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-07-30 17:25:48 +0300

    task 0 done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
-const filename = process.argv[2];
 const fs = require('fs');
-fs.readFile(filename, 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+const file = process.argv[2];
+try {
+  const data = fs.readFileSync(file, 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
